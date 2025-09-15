099b69734d414840187ef3f49ed74cb6d3b446f5
Nancy Beaula <nancyebinezer@gmail.com>
2025-09-14 20:04:45 -0400

    IfElseSwitch program for understanding

---
+++ src/main/java/IfElseSwitch.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class IfElseSwitch {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int number = scanner.nextInt();
+
+        if (number % 5 == 0 && number % 3 == 0)
+            System.out.println("FizzBuzz");
+        else if (number % 3 == 0)
+            System.out.println("Buzz");
+        else if (number % 5 == 0)
+            System.out.println("Fizz");
+        else
+            System.out.println(number);
+
+    }
+
+
+
+}
